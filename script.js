d4a5707bc2c2454f735b7c0caa7c677c0a9747f9
TimotyMercads <timotymm05@gmail.com>
2025-10-29 11:08:23 +0800

    Re-added the removed semicolons

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const info = document.createElement("div");
   info.style.opacity = "0.9";
   info.style.fontSize = "0.9rem";
-  info.style.margin = "16px 0"
+  info.style.margin = "16px 0";
   info.textContent =
     "This page is a static demo. The JS has an intentional linting issue (missing semicolon).";
-  document.querySelector(".wrap").appendChild(info)
-})
+  document.querySelector(".wrap").appendChild(info);
+});
